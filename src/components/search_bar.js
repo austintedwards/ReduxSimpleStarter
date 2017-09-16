de4f924675d2449ba3107311325c3740b3088d70
austintedwards <austintedwards@gmail.com>
2017-09-16 16:33:40 -0600

    adding event

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // create a new componet with HTML
-const SearchBar = ()=>{
-  return <input />
+class SearchBar extends Component{
+  render(){
+    return <input onChange = {this.onInputChange}/>
+  }
+  onInputChange(event){
+  }
 }
 
 
