bf9d7c4a2470e2af349d92569556c6f88d4ca054
austintedwards <austintedwards@gmail.com>
2017-09-16 16:23:53 -0600

    new files working

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// create a new componet with HTML
+const SearchBar = ()=>{
+  return <input />
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = "AIzaSyCcCc1PNHYLw2pVHikkVHnBLCAOQGer94Q";
+
+
 // create a new componet with HTML
 const App = ()=>{
-  return <div>HI!</div>
+  return (<div>
+    <SearchBar />
+  </div>
+  )
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
