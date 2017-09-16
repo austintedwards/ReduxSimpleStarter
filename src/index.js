903a5d82b715495f5e4b85377d8a008b0a967650
austintedwards <austintedwards@gmail.com>
2017-09-16 17:51:19 -0600

    getting video array

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = "AIzaSyCcCc1PNHYLw2pVHikkVHnBLCAOQGer94Q";
 
 
+
 // create a new componet with HTML
-const App = ()=>{
-  return (<div>
-    <SearchBar />
-  </div>
-  )
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={videos: []}
+
+    YTSearch({key: API_KEY, term: 'Trixie Mattel'}, (videos) =>{
+      this.setState({videos:videos});
+    })
+  }
+  render(){
+    return (
+      <div>
+      <SearchBar />
+      <VideoList videos = {this.state.videos}/>
+    </div>
+  );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
