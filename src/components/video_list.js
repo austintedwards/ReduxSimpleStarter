8db5b339e81655eab2c07bf39b7dc043738610c7
austintedwards <austintedwards@gmail.com>
2017-09-16 18:03:20 -0600

    video list appearing

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import VideoListItem from './video_list_item'
+
 
 const VideoList = (props)=>{
+  console.log(props)
+
+const videoItems = props.videos.map((video)=>{
+  return <VideoListItem key = {video.etag} video ={video} />
+})
+
   return (
     <ul className = "col-md-4 videos">
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 }
---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoListItem = ({video}) =>{
+  console.log(video)
+  const imgURL = video.snippet.thumbnails.default.url
+  return (
+        <li className = "list-group-item">
+          <div className = "video-list media">
+            <div className = "media-left">
+              <img className ="media-object" src ={imgURL}/>
+            </div>
+          </div>
+
+          <div className = "media-body">
+            <div className = "media-heading">
+              {video.snippet.title}
+            </div>
+          </div>
+      </li>
+    )
+}
+
+export default VideoListItem;
