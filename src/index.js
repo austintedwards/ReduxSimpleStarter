359f8eaaa3757073b4e4f04a5ebaa99b530633de
austintedwards <austintedwards@gmail.com>
2017-09-16 20:21:26 -0600

    video selector working

--- src/index.js
+++ src/index.js
@@ -13,18 +13,25 @@ const API_KEY = "AIzaSyCcCc1PNHYLw2pVHikkVHnBLCAOQGer94Q";
 class App extends Component{
   constructor(props){
     super(props);
-    this.state={videos: []}
+    this.state={videos: [],
+    selectedVideo:null
+    }
 
     YTSearch({key: API_KEY, term: 'Trixie Mattel'}, (videos) =>{
-      this.setState({videos:videos});
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      });
     })
   }
   render(){
     return (
       <div>
       <SearchBar />
-      <VideoDetail video={this.state.videos[0]}/>
-      <VideoList videos = {this.state.videos}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+        videos = {this.state.videos}/>
     </div>
   );
   }
